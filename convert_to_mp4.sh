9c18bd7edec2178365c95390178b26432049a04a
tknr <info@tknr.com>
2017-09-19 20:40:01 +0900

    2017年  9月 19日 火曜日 20:40:01 JST

--- convert_to_mp4.sh
+++ convert_to_mp4.sh
@@ -15,17 +15,19 @@ fi
 
 
 FFMPEG=/usr/local/bin/ffmpeg
-proc=`cat /proc/cpuinfo | grep processor | wc -l`
-let procnum="${proc} / 2";
-EXTLIST=("mkv" "flv" "rmvb" "wmv" "M4A" "avi")
+#proc=`cat /proc/cpuinfo | grep processor | wc -l`
+#let procnum="${proc} / 2";
+EXTLIST=("mpeg" "mpg" "mkv" "flv" "rmvb" "wmv" "M4A" "avi")
 for DIR in ${argv}
 do
 	for EXT in ${EXTLIST[@]}
 	do
-		for FILENAME in `find "${DIR}" -name "*.${EXT}"`
+		for FILENAME in `find "${DIR}" -name "*.${EXT}" | sort`
 		do
-			echo ${FILENAME}
-			/bin/nice -n 19 ${FFMPEG} -y -i "${FILENAME}" -threads ${procnum} "${FILENAME%.${EXT}}.mp4" || continue
+			ls -lh ${FILENAME}
+#			/bin/nice -n 19 ${FFMPEG} -y -i "${FILENAME}" -threads ${procnum} "${FILENAME%.${EXT}}.mp4" || continue
+			/bin/nice -n 19 ${FFMPEG} -y -i "${FILENAME}" "${FILENAME%.${EXT}}.mp4" || continue
+			ls -lh "${FILENAME%.${EXT}}.mp4"
             		rm -f "${FILENAME}"
 		done
 	done
